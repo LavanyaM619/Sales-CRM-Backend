4d3b863e0353b2371442cccd6f920f11249918bf
LavanyaM619 <lalumogan619@gmail.com>
2025-09-04 10:04:09 +0530

    testing

--- models/Counter.js
+++ models/Counter.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 const counterSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
   seq: { type: Number, default: 0 }
-});
+});//counter
 
 export default mongoose.model("Counter", counterSchema);
